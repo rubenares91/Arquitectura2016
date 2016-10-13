e8249e57f5dd8252881c83b5892e4cb82687affa
rares <rares@10.164.52.232>
2016-10-13 18:31:33 +0200

    Commit 13/10

--- Bolsa2016/src/prueba/prueba.java
+++ Bolsa2016/src/prueba/prueba.java
@@ -2,4 +2,11 @@ package prueba;
 
 public class prueba {
 
+	@Override
+	public String toString() {
+		return "prueba [getClass()=" + getClass() + ", hashCode()=" + hashCode() + ", toString()=" + super.toString()
+				+ "]";
+	}
+
+
 }
