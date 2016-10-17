c8cd8c9497b660145c38f47a42eee84876dc0a86
Usuario <Usuario@10.164.51.229>
2016-10-17 18:47:06 +0200

    segunda modificacion

--- Bolsa2016/src/pro/arq/dao/clases/Cartera.java
+++ Bolsa2016/src/pro/arq/dao/clases/Cartera.java
@@ -1,9 +1,48 @@
 package pro.arq.dao.clases;
 
+
 public class Cartera {
 
-	public Cartera() {
-		// TODO Auto-generated constructor stub
-	}
+private String dni;
+private String empresa;
+private boolean compraVenta;
+private int cantidad;
+
+
+public Cartera(){
+	super();
+}
+public Cartera(String dni, String empresa, boolean compaVenta,int cantidad) {
+	super();
+	this.dni = dni;
+	this.empresa = empresa;
+	this.compraVenta = compaVenta;
+	this.cantidad = cantidad;
+	
+}
+public String getDni() {
+	return dni;
+}
+public void setDni(String dni) {
+	this.dni = dni;
+}
+public String getEmpresa() {
+	return empresa;
+}
+public void setEmpresa(String empresa) {
+	this.empresa = empresa;
+}
+public boolean getCompraVenta() {
+	return compraVenta;
+}
+public void setcompraVenta(boolean compraVenta) {
+	this.compraVenta = compraVenta;
+}
+public int getCantidad() {
+	return cantidad;
+}
+public void setCantidad(int cantidad) {
+	this.cantidad = cantidad;
+}
 
 }
--- Bolsa2016/src/pro/arq/dao/clases/Peticion.java
+++ Bolsa2016/src/pro/arq/dao/clases/Peticion.java
@@ -2,8 +2,46 @@ package pro.arq.dao.clases;
 
 public class Peticion {
 
-	public Peticion() {
-		// TODO Auto-generated constructor stub
-	}
+private String dni;
+private String empresa;
+private boolean compraVenta;
+private int cantidad;
+
+
+public Peticion(){
+	super();
+}
+public Peticion(String dni, String empresa, boolean compaVenta,int cantidad) {
+	super();
+	this.dni = dni;
+	this.empresa = empresa;
+	this.compraVenta = compaVenta;
+	this.cantidad = cantidad;
+	
+}
+public String getDni() {
+	return dni;
+}
+public void setDni(String dni) {
+	this.dni = dni;
+}
+public String getEmpresa() {
+	return empresa;
+}
+public void setEmpresa(String empresa) {
+	this.empresa = empresa;
+}
+public boolean getCompraVenta() {
+	return compraVenta;
+}
+public void setcompraVenta(boolean compraVenta) {
+	this.compraVenta = compraVenta;
+}
+public int getCantidad() {
+	return cantidad;
+}
+public void setCantidad(int cantidad) {
+	this.cantidad = cantidad;
+}
 
 }
